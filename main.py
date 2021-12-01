9cae124a230c0695ac0021fb75da99b62f210d69
00012419 <wiut00012419@gmail.com>
2021-12-01 15:57:34 +0500

    First part

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("hello lanati world")
+def ask():
+    is_alpha = True
+    while is_alpha:
+        yes_no = input("Value : ")
+        if yes_no.lower() == "yes":
+            is_alpha = False
+            return True
+        elif yes_no.lower() == "no":
+            is_alpha = False
+            return False
+        else:
+            print("Please, Enter yes or no only")
